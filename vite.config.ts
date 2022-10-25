c9157d3b690d508e3427c74955b496701d279483
Thomas Card <tcardlab@gmail.com>
2022-10-25 14:27:03 -0400

    Init: destructure props

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 
 import solid from "solid-start/vite";
 import solidStyled from 'vite-plugin-solid-styled';
+import { undestructurePlugin } from "babel-plugin-solid-undestructure"
 
 import Unocss from 'unocss/vite'
 import { presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
   plugins: [
+    ...undestructurePlugin('ts'),
     solid(),
     solidStyled({
       filter: {
