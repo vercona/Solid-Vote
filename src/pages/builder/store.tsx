bc7be97a24c163e658acf43078339da7137d4eb3
Thomas Card <tcardlab@gmail.com>
2022-10-29 20:32:16 -0400

    Checkpoint: init most options

--- src/pages/builder/store.tsx
+++ src/pages/builder/store.tsx
@@ -1,7 +1,7 @@
 import { createStore } from "solid-js/store";
 
 /* let components = {
-  collapese: Collapese
+  category: Collapse
 } */
 
 export default function useStore() {
@@ -30,128 +30,113 @@ export default function useStore() {
       // maybe have this be a major set w/ appropriate vote/calc types 
       content: [
         'Threshold', // if threshold reached, deem winner (freeze or continue voting? post-win-event handling?)
+        'Random'
       ]
     },
 
     {
       label: 'Vote Type', type: 'category',
       content: [
-        'Manual',    // users have a vote interface
-        'Implicit',  // submission-properties api used (view count etc.)
-        'External'   // some other api use to determine winners
+        'Manual',     // users have a vote interface
+        'Implicit',   // submission-properties api used (view count etc.)
+        'External',   // some other api use to determine winners
+        'Random',     // Winner(s) selected at random
+      ]
+    },
+
+    {
+      label: 'Input Options', type: 'category',
+      // if manual
+      content: [
+        'Upvote',
+        'Upvote + Downvote',
+
+        'integer Rating [input min,max]',
+        'percentage Allocation',
+        'Star Rating [5,10]',
+
+        'ranking',
+        'tier lists',
+
+        'binary comparison',
+
+        'tag vote [tags] (like emote reactions)'
+      ]
+    },
+
+    {
+      label: 'Input Limit', type: 'category',
+      // if manual
+      content: [
+        'unlimited',
+        'limited [input]',
+        'Dynamic [function api(views, submissions)]'
+      ]
+    },
+
+    {
+      label: 'Implicit Options', type: 'category',
+       // if implicit (may need api and callback bs cuz this is pretty project specific)
+      content: [
+        'time interactive/engagement',
+        'view count',
+        'edit count',
+        'comment count',
+        'content',
+      ]
+    },
+
+    {
+      label: 'External Options', type: 'category',
+      // if external: connects to external api...
+      content: [ ]
+    },
+
+    {
+      label: 'Calculation Type', type: 'category',
+      content: [
+        'Provided',  // use one of our provided methods 
+        'External',  // some other api use to determine winners
+        'Custom'     // pass callback function
+      ]
+    },
+  
+    {
+      label: 'Calculation Options', type: 'category',
+      // if Provided. perhaps use type matching for first pass simple validation
+      content: [
+        'unanimous',
+        'Majority rule',
+        'Super Majority {"50 > input < 100"}',
+        'plurality',
+        'ranked choice / instant runoff',
+        'up-vote/score approval voting',
+        'diminishing/depreciating vote (the more you vote the less they are worth, uniformly)',
+        'single transferable vote',
+        'weighted random',
       ]
     }
-
-    
-    
   ]
 }
 
+
 /*
+## Extensions Beyond Vote Builder
+  - All submissions hidden, click to reveal, timer starts
+  based on size of submission, determine how long it would take to review
+  after timer hits that threshold, a usr can vote for it.
 
-## Options
-
-- All submissions hidden, click to reveal, timer starts
-based on size of submission, determine how long it would take to review
-after timer hits that threshold, a usr can vote for it.
-
-- Hide Other vote counts
+  - Hide Other vote counts
 
 
-event based vs continuous threshold based
+  vote limit pass through to children
 
-multi winner single winner
+  workflow: pass results of one vote onto another vote. can simulate a republic this way
+  <Collapse title="Edit Level">
+    <p>Inherit Submission Level</p>
+  </Collapse>
 
-random 
-
-number of winners
-
-vote limit pas through to children
-
-workflow: pass results of one vote onto another vote. can simulate a republic this way
-
-
-import './style.scss'
-
-import Collapse from "~/components/Collapse";
-import Page from "~/components/Page";
-
-
-tier lists
-
-export default function builder () {
-  return (
-    <Page title="Vote Builder"> 
-      <div>
-        <Collapse title="Submission" >
-          <Collapse title="Vote Type:" h="4">
-            radio:<br/>
-              • Manual<br/>
-              • Implicit<br/>
-              • External<br/>
-            <br/>
-
-            If Manual: <br/>
-              • Upvote <br/>
-              • Upvote + Downvote <br/>
-
-              • integer Rating [input min,max] <br/>
-              • percentage Allocation <br/>
-              • Star Rating [5,10] <br/>
-
-              • ranking<br/>
-
-              • binary comparison<br/>
-
-              • none (voting not allowed)<br/>
-              • tag vote [tags] (like emote reactions)<br/>
-
-            <br/>
-            If Implicit:<br/>
-              • time interactive/engagement<br/>
-              • view count<br/>
-              • edit count<br/>
-              • comment count<br/>
-              • content
-
-            <br/>
-            If External(/functional?):<br/>
-              • API
-          </Collapse>
-
-          <Collapse title="Vote Limit:" h="4">
-            radio: <br/>
-              • unlimited <br/>
-              • limited [input]<br/>
-              • Dynamic [function api(views, submissions)]<br/>
-          </Collapse>
-
-          <Collapse title="Calculation:" h="4">
-            radio: <br/>
-              • unanimous<br/>
-              • Majority rule<br/>
-              • Super Majority {"50 > input < 100"}<br/>
-              • plurality<br/>
-              • ranked choice / instant runoff<br/>
-              • up-vote/score approval voting <br/>
-              • deprecating vote (the more you vote the less they are worth, uniformly)<br/>
-              • weighted random<br/>
-              • single transferable vote<br/>
-          </Collapse>
-          
-        </Collapse>
-
-        <Collapse title="Edit Level">
-          <p>Inherit Submission Level</p>
-        </Collapse>
-
-        <Collapse title="Comment Level">
-          <p>Inherit Submission Level</p>
-        </Collapse>
-      </div>
-    </Page>
-  )
-}
-
-
-*/
+  <Collapse title="Comment Level">
+    <p>Inherit Submission Level</p>
+  </Collapse>
+*/ 
