3597c3f2e231faf20eb95a644cb9cfc4f0524cbe
Thomas Card <tcardlab@gmail.com>
2022-11-20 20:24:58 -0500

    Chore: clean

--- src/pages/builder/index.tsx
+++ src/pages/builder/index.tsx
@@ -8,50 +8,9 @@ import formParser from '~/use/formParser'
 import valueArr from '~/static/keyArr.json'
 import showArr from '~/static/showArr.json' 
 
-/* interface Form {
-  label: 'Submission Settings',
-  type: 'category',
-  content: [],
-} */
-
-
-
-
 export default function builder () {
   let arr = useStore()
-  /* let arr = [
-    {
-      label: 'Submission Settings', type: 'category',
-      content: [
-        {
-          label: 'Submission Limit', type: 'text',
-        },
-        {
-          label: 'Submission Vitality', type: 'radio',
-          options: [
-            'Expiration',
-            'lock on win'
-          ]
-        }
-      ]
-    },
 
-    {
-      label: 'HMM', type: 'category',
-      content: [
-        {
-          label: 'Deeper!', type: 'category',
-          h: '4',
-          content: [
-            {
-              label: 'another input', type: 'text',
-            }
-          ]
-        },
-      ]
-    }
-  ]
- */
   return (
     <Page
       title="Vote Builder"
